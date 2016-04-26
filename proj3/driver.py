edd5d3b81a5afaa26d33b9475b63639949de807a
sieabah <christopher.sidell@gmail.com>
2016-04-26 19:42:01 -0400

    Use tests folder

--- proj3/driver.py
+++ proj3/driver.py
@@ -13,8 +13,11 @@ def main():
     imgFp = sys.argv[1]
 
     if imgFp == 'ALL':
-        for fp in os.listdir('.'):
+        testdir = 'tests'
+        for fp in os.listdir(testdir):
             if fp.endswith('.jpg') or fp.endswith('.png'):
+                name = fp
+                fp = os.path.join(testdir, fp)
                 print(fp, 'is found to be a', find_most_similar(Image.open(fp)))
         sys.exit(0)
 
